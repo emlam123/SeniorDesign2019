a51aac8c9ac463ce395c5913d050aa66b9843b7a
Josh Lee <hongjul@uci.edu>
2019-11-14 03:18:16 -0800

    made a function to read and store the data into a queue. When the queue is full, it saves/writes the stored values into an output csv file.

--- open.py
+++ open.py
@@ -1,39 +1,34 @@
 import csv
+import queue
 
-<<<<<<< HEAD
-def get_muscle_data():
-    ''' it opens the csv file and sends the data to the client.'''
-=======
+MEMORYSIZE = 35
+STORAGE = queue.Queue(maxsize=MEMORYSIZE)
 
 def get_muscle_data():
     '''
-    it opens the csv file and sends the data to the client.
-        the given file has 64 columns containing - 8 consecutive readings from 8 sensors. (8 X 8)
+    it opens the csv file and stores the data into a queue.
+        When the queue is full, it writes its value to the output csv file then empties the queue.
+            the input file has 64 columns containing - 8 consecutive readings from 8 sensors. (8 X 8)
     '''
->>>>>>> 09a475b0cd1c3c2f7a51f9da16b0ede4f29ade86
+
     with open('0.csv', newline='') as csvfile:
         filereader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         linecount = 0
+        colcount = 0;
         for row in filereader:
-<<<<<<< HEAD
-            if(linecount == 0):
+            if(linecount < 100):
                 linecount += 1
-                print(str(linecount)+ ' : The columns')
-                print(', '.join(row))
-            elif(linecount < 5 and linecount >0):
-=======
-            if linecount == 0:
-                linecount += 1
-                print(str(linecount) + ' : The columns')
-                print(', '.join(row))
-            elif linecount < 5:
->>>>>>> 09a475b0cd1c3c2f7a51f9da16b0ede4f29ade86
-                linecount += 1
-                print(linecount)
-                print(', '.join(row))
+                if not STORAGE.full():
+                    print(row[0].split(',')[0])
+                    STORAGE.put(row[0].split(',')[0])
+                else:
+                    with open('outputData.csv', 'a') as fd:
+                        while not STORAGE.empty():
+                            fd.write(STORAGE.get()+'\n')
 
 
-<<<<<<< HEAD
+
+
 
 def Main():
 
@@ -42,11 +37,6 @@ def Main():
 
 
 
-
-=======
-def Main():
-    get_muscle_data()
->>>>>>> 09a475b0cd1c3c2f7a51f9da16b0ede4f29ade86
 
 
 if __name__ == '__main__':
