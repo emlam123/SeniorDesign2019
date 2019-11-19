5ab21f16eaeb7abdfe92b9388bd33edb46933079
martin <trinidadmartin808@gmail.com>
2019-11-18 20:39:00 -0800

    "Server vamped"

--- server.py
+++ server.py
@@ -9,14 +9,17 @@ print_lock = threading.Lock()
 
 
 def threaded(c):
-    heart_rate_queue = []
+    heartrate_queue = [None] * 30
     speed_queue = [None] * 30
-    force_queue = []
-    speed_pointer = 0
-    heart_rate_pointer = 0
-    speed_pointer = 0
-    speed_sum = 0
+    force_queue = [None] * 30
 
+    heartrate_pointer = 0
+    speed_pointer = 0
+    force_pointer = 0
+
+    heartrate_sum = 0
+    speed_sum = 0
+    force_sum = 0
     while True:
 
         data = c.recv(1024)
@@ -25,36 +28,76 @@ def threaded(c):
             sys.exit(0)
         message = str(data.decode('ascii'))
 
+        # Speed Sensor is flag 1
         if message[0] == "1":
             message = message.replace("1:", '')
             print("Received at: " + str(datetime.datetime.now().time()))
             message = message.split("\n")
             if speed_pointer == 29:
                 print("Writing to CSV file")
-                speed_queue[speed_pointer] = [message[0], str(datetime.datetime.now().time())]
+                speed_queue[speed_pointer] = [message[0], str(datetime.datetime.now().time()), message[1]]
 
-                with open('speed_rate.csv', 'a') as f:
+                with open('speed_rate.csv', 'a', newline='') as f:
                     writer = csv.writer(f)
                     for data in speed_queue:
                         speed_sum = speed_sum + int(data[0])
                         writer.writerow(data)
 
-                with open('average_speed.csv', 'a') as f:
+                with open('average_speed.csv', 'a', newline='') as f:
                     hello = [float(speed_sum/30),str(datetime.datetime.now().time())]
                     writer = csv.writer(f)
                     writer.writerow(hello)
                 speed_sum = 0
                 speed_pointer = 0
-            speed_queue[speed_pointer] = [message[0],str(datetime.datetime.now().time())]
+            speed_queue[speed_pointer] = [message[0],str(datetime.datetime.now().time()),message[1]]
             speed_pointer = speed_pointer + 1
 
+        # Force Sensor is flag 2
         if message[0] == "2":
-            message = "Received force rate: " + message.replace("2:", '')
-            print("Received at: " + str(datetime.datetime.now()))
+            message = message.replace("2:", '')
+            print("Received at: " + str(datetime.datetime.now().time()))
+            message = message.split("\n")
+            if force_pointer == 29:
+                print("Writing to CSV file")
+                force_queue[force_pointer] = [message[0], str(datetime.datetime.now().time()), message[1]]
+
+                with open('force_rate.csv', 'a', newline='') as f:
+                    writer = csv.writer(f)
+                    for data in force_queue:
+                        force_sum = force_sum + int(data[0])
+                        writer.writerow(data)
+
+                with open('average_force.csv', 'a', newline='') as f:
+                    hello = [float(force_sum/30),str(datetime.datetime.now().time())]
+                    writer = csv.writer(f)
+                    writer.writerow(hello)
+                force_sum = 0
+                force_pointer = 0
+            force_queue[force_pointer] = [message[0],str(datetime.datetime.now().time()),message[1]]
+            force_pointer = force_pointer + 1
 
         if message[0] == "3":
-            message = "Received heart rate(BPM) rate: " + message.replace("3:", '')
-            print("Received at: " + str(datetime.datetime.now()))
+            message = message.replace("3:", '')
+            print("Received at: " + str(datetime.datetime.now().time()))
+            message = message.split("\n")
+            if heartrate_pointer == 29:
+                print("Writing to CSV file")
+                heartrate_queue[heartrate_pointer] = [message[0], str(datetime.datetime.now().time()), message[1]]
+
+                with open('heart_rate.csv', 'a', newline='') as f:
+                    writer = csv.writer(f)
+                    for data in heartrate_queue:
+                        heartrate_sum = heartrate_sum + int(data[0])
+                        writer.writerow(data)
+
+                with open('average_heartrate.csv', 'a', newline='') as f:
+                    hello = [float(heartrate_sum/30),str(datetime.datetime.now().time())]
+                    writer = csv.writer(f)
+                    writer.writerow(hello)
+                heartrate_sum = 0
+                heartrate_pointer = 0
+            heartrate_queue[heartrate_pointer] = [message[0],str(datetime.datetime.now().time()),message[1]]
+            heartrate_pointer = heartrate_pointer + 1
 
 
         c.send("Server Received at".encode('ascii'))
@@ -65,10 +108,10 @@ def threaded(c):
 
 def Main():
     try:
-        host = "192.168.1.40"
+        host = "100.67.113.183"
         port = 12345
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('192.168.1.40', port))
+        s.bind(('100.67.113.183', port))
         print("socket binded to port", port)
 
         s.listen(5)
